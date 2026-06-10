5daa895652613f04350ac2e5bae06da5af831f1f
Pedro-areis <pedrogiarola1@gmail.com>
2026-06-09 22:20:59 -0300

    Initial Commit

---
+++ src/main/java/dev/pedroreis/rest_skeleton_java/RestSkeletonJavaApplication.java
@@ -0,0 +1,13 @@
+package dev.pedroreis.rest_skeleton_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSkeletonJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSkeletonJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/pedroreis/rest_skeleton_java/RestSkeletonJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pedroreis.rest_skeleton_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSkeletonJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
