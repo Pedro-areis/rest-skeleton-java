42efba2666458bcd014e0106494b3fecddeb97b4
Pedro-areis <pedrogiarola1@gmail.com>
2026-06-11 21:38:09 -0300

    refactor(user): update class user for follow the hexagonal architecture

--- src/main/java/dev/pedroreis/rest_skeleton_java/user/domain/User.java
+++ src/main/java/dev/pedroreis/rest_skeleton_java/user/domain/User.java
@@ -10,9 +10,6 @@ public class User {
     private String password;
     private LocalDateTime createdAt;
 
-    public User() {
-    }
-
     public User (UUID id, String name, String email, String password, LocalDateTime createdAt) {
         this.id = id;
         this.name = name;
@@ -27,10 +24,11 @@ public class User {
         }
     }
 
-    public User (String email, String password) {
+    public User (String name, String email, String password) {
         validateEmail(email);
 
         this.id = UUID.randomUUID();
+        this.name = name;
         this.email = email;
         this.password = password;
         this.createdAt = LocalDateTime.now();
@@ -41,10 +39,4 @@ public class User {
     public String getEmail () { return email; }
     public String getPassword () { return password; }
     public LocalDateTime getCreatedAt () { return createdAt; }
-
-    public void setId (UUID id) { this.id = id; }
-    public void setName (String name) { this.name = name; }
-    public void setEmail (String email) { this.email = email; }
-    public void setPassword (String password) { this.password = password; }
-    public void setCreatedAt (LocalDateTime createdAt) { this.createdAt = createdAt; }
 }
