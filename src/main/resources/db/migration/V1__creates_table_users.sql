19d1e0d824a44dfdb28c765bddd9b2e8c36f0e4f
Pedro-areis <pedrogiarola1@gmail.com>
2026-06-18 21:51:09 -0300

    feature(user): corrects the gen_random_uuid() function from migration V1.

--- src/main/resources/db/migration/V1__creates_table_users.sql
+++ src/main/resources/db/migration/V1__creates_table_users.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (
-    id UUID PRIMARY KEY DEFAULT gen_ramdom_uuid(),
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL,
